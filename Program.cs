149633f0d410e2a9cd80805293156321810bfcb1
Paul Miller <pmiller@microsoft.com>
2016-04-20 14:08:47 -0700

    try cmdlet

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using System.Management.Automation;  // Windows PowerShell assembly.
+
 
 namespace out_blob
 {
@@ -12,14 +14,53 @@ namespace out_blob
     {
         static void Main(string[] args)
         {
-            var account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("outblobaccount"));
-            var container = account.CreateCloudBlobClient().GetContainerReference("outblob");
-            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
-            var blob = container.GetAppendBlobReference(Guid.NewGuid().ToString());
-            Console.WriteLine(blob.Uri);
-            if (!blob.Exists()) blob.CreateOrReplace();
             
-            blob.AppendFromStream(Console.OpenStandardInput());
+        
         }
     }
+    
+   
+namespace OutBlob
+{
+  // Declare the class as a cmdlet and specify and 
+  // appropriate verb and noun for the cmdlet name.
+  [Cmdlet(VerbsCommunications.Out, "Blob")]
+  public class OutBlobCommand : Cmdlet
+  {
+    // Declare the parameters for the cmdlet.
+     [Parameter(
+        Position = 0,
+        ValueFromPipeline = true,
+        ValueFromPipelineByPropertyName = true
+    )]
+    [ValidateNotNullOrEmpty]
+    public Object[] Inputs
+    {
+      get;
+      set;
+    }
+    private CloudAppendBlob blob;
+    
+    public override BeginProcessing()
+    {
+        var account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("outblobaccount"));
+        var container = account.CreateCloudBlobClient().GetContainerReference("outblob");
+        container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+        blob = container.GetAppendBlobReference(Guid.NewGuid().ToString());
+        if (!blob.Exists()) blob.CreateOrReplace();            
+        Console.WriteLine(blob.Uri);
+    }
+
+    // Overide the ProcessRecord method to process
+    // the supplied user name and write out a 
+    // greeting to the user by calling the WriteObject
+    // method.
+    protected override  ProcessRecord()
+    {
+        Console.WriteLine($"input count {Inputs.Length}");
+        foreach (var i in Inputs)
+            blob.AppendFromText(i.ToString());
+    }
+  }
+}
 }
