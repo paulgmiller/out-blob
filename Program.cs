28a0713c3a0ac4a744357f1f3a4c8c82a5e78476
Paul Miller <paul.miller@gmail.com>
2016-04-19 23:28:21 -0700

    do something maybe useful

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace out_blob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("outblobaccount"));
+            var container = account.CreateCloudBlobClient().GetContainerReference("outblob");
+            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+            var blob = container.GetAppendBlobReference(Guid.NewGuid().ToString());
+            Console.WriteLine(blob.Uri);
+            if (!blob.Exists()) blob.CreateOrReplace();
+            
+            blob.AppendFromStream(Console.OpenStandardInput());
+        }
+    }
+}
